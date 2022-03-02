814bfbb0059da4160e51ed0c6ff77a423fe085e5
yuvraj3905 <yuvraj202001@gmail.com>
2022-03-02 16:42:35 +0530

    1

--- Graphs1/dfs.cpp
+++ Graphs1/dfs.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int print(int**edges,int n,int sv){          //sv=starting vertex
-       
-}
+void print(int**edges,int n,int sv,bool* visited) {          //sv=starting vertex
+     cout<<sv<<endl;
+     visited[sv] = true;
+     for(int i=0;i<n;i++){
+         if(i==sv){
+             continue;
+         }
+         if(edges[sv][i]==1){
+             if(visited[i]){
+                 continue;
+             }
+             print(edges,n,i,visited);
+         }
+    }
+}  
+
 
 int main(){
 
@@ -22,5 +35,9 @@ int main(){
         edges[f][s]=1;
         edges[s][f]=1;
         }
-        print(edges,n,0);
+        bool* visited=new bool[n];
+        for(int i=0;i<n;i++){
+            visited[i]=false;
+        }
+        print(edges,n,0,visited);
 }
