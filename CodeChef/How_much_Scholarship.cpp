9f2e9a2116743234819d07736be8a8d6ad1f8866
yuvraj3905 <yuvraj202001@gmail.com>
2022-03-17 21:58:53 +0530

    Update How_much_Scholarship.cpp

--- CodeChef/How_much_Scholarship.cpp
+++ CodeChef/How_much_Scholarship.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main(){
     int x;
-    i
+    if(x<=50){
+        cout << "100";
+    }else if(x<=100){
+        cout << "50";
+    }else{
+        cout << "0";
+    }
 }
