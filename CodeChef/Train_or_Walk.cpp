a7530cfda185520eb845904974f83c9bdb5add60
yuvraj3905 <yuvraj202001@gmail.com>
2022-03-18 15:33:25 +0530

    8.1

---
+++ CodeChef/Train_or_Walk.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int N,A,B,C,P,Q,Y;
+        cin >> N >> A >> B >> C >> P >> Q>>Y;
+        for(int i=0; i <n,i++){
+            cin >>i;
+            
+        }
+    }
+}
